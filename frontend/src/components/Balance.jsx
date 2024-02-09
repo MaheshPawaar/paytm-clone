99206d71bd5940d313bb27eecbcba91777d08338
Mahesh Pawar <pawarmahesh723@gmail.com>
2024-02-10 00:28:53 +0530

    Implement Dashboard page

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,3 +1,8 @@
-export function Balance() {
-  return <div></div>
+export function Balance({ value }) {
+  return (
+    <div className="flex">
+      <div className="font-bold text-lg">Your balance</div>
+      <div className="font-semibold ml-4 text-lg">Rs. {value}</div>
+    </div>
+  );
 }

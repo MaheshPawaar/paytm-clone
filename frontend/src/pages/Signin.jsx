86c1131b999c95c207cf13adc60542f5ed24521a
Mahesh Pawar <pawarmahesh723@gmail.com>
2024-02-09 23:58:38 +0530

    Refactor Signin Page

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -19,6 +19,7 @@ export const Signin = () => {
           <BottomWarning
             label={"Don't have an account"}
             buttonText={'Sign up'}
+            to={'/signup'}
           />
         </div>
       </div>
